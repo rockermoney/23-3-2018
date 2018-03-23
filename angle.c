d6e589eff3cf32d94a815fbbee8b3b1dafad2f5b
rockermoney <35295528+rockermoney@users.noreply.github.com>
2018-03-23 15:52:38 +0530

    Update angle.c

--- angle.c
+++ angle.c
@@ -2,11 +2,11 @@
 #include<math.h>
 void main()
 {
-	float pi=3.14,value,r;
-	int n;
-	printf("Enter the Sine Value:");
-	scanf("%d",&n);
-	value=pi/180;
-	r=sin(n*value);
-	printf("\n%f",r);
+float pi=3.14,value,r;
+int n;
+printf("Enter the Sine Value:");
+scanf("%d",&n);
+value=pi/180;
+r=sin(n*value);
+printf("\n%f",r);
 }
